a07e1101812d51434dee05324c27016a7fa7cb66
janki2210 <80520217+janki2210@users.noreply.github.com>
2021-10-14 17:33:09 +0530

    main

--- js/indexs.js
+++ js/indexs.js
@@ -3,3 +3,19 @@ $(document).ready(function() {
         $('ul').toggleClass('show');
     });
 });
+
+$(document).ready(function() {
+    $('.btn').click(function() {
+      const value = $(this).attr('data-filter');
+      if(value == 'all'){
+          $('.portfoliobox').show('1000');
+      }
+      else{
+        $('.portfoliobox').not('.'+value).hide('1000'); 
+        $('.portfoliobox').filter('.'+value).show('1000'); 
+      }
+    });
+    $('.portfoliobox').click(function(){
+        $(this).addclass('active').siblings().removeclass('active');
+    });
+});
